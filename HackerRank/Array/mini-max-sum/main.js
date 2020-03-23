2ce63961b74acfb0784a46a7ff812d0a1521bf18
timothywlee <timothylee3508@gmail.com>
2020-03-23 05:50:54 +0000

    completed challenge

--- HackerRank/Array/mini-max-sum/main.js
+++ HackerRank/Array/mini-max-sum/main.js
@@ -1,4 +1,13 @@
 function miniMaxSum(arr) {
+  let min = null;
+  let max = null;
+  let sum = null;
+  let array = [];
 
+  arr = arr.sort();
+  array = arr.reduce((a, b) => a + b);
+  min = array - arr[arr.length - 1];
+  max = array - arr[0];
 
+  console.log(`${min} ${max}`)
 }
