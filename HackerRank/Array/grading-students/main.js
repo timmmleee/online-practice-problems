e90abcb7e0e916d243a2f1c9692db099f528b41c
timothywlee <timothylee3508@gmail.com>
2020-03-24 21:43:00 +0000

    completed challenge

--- HackerRank/Array/grading-students/main.js
+++ HackerRank/Array/grading-students/main.js
@@ -1,4 +1,15 @@
 function gradingStudents(grades) {
-  // Write your code here
+  for (let i = 0; i < grades.length; i++) {
+    let roundedUpNum = Math.ceil(grades[i] / 5) * 5;
+    let difference = roundedUpNum - grades[i];
 
+    if (difference < 3 && grades[i] >= 38) {
+      grades[i] = roundedUpNum;
+    } else if (difference === 3 && grades[i] >= 38) {
+      grades[i] = grades[i]
+    } else if (grades[i] < 38) {
+      grades[i] = grades[i]
+    }
+  }
+  return grades;
 }
