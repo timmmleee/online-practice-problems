7441f03fdd9dd194bd8f57b0e787e95cba278e9e
timothywlee <timothylee3508@gmail.com>
2020-04-25 06:56:18 +0000

    Completed Challenge

--- LeetCode/implement-strStr/main.js
+++ LeetCode/implement-strStr/main.js
@@ -1,3 +1,9 @@
 var strStr = function (haystack, needle) {
-
+  let indexPosition = null;
+  if (needle == ''){
+    indexPosition = 0;
+  } else {
+    indexPosition = haystack.indexOf(needle);
+  }
+  return indexPosition;
 };
