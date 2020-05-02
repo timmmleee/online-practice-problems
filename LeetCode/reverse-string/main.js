dccb07e724b7ca2e1c592923c3a8503e216a8a28
timothywlee <timothylee3508@gmail.com>
2020-05-02 06:05:46 +0000

    Completed challenge and added other ways to solve problem

--- LeetCode/reverse-string/main.js
+++ LeetCode/reverse-string/main.js
@@ -1,3 +1,37 @@
 var reverseString = function (s) {
-
+  s.reverse();
 };
+
+//Use reverseString(s) if s is equal to an array with elements
+
+/*
+Problem:
+Reverse the sentence, "Hi, my name is Tim".
+Is the sentence hello a string?
+Do I include the whitespaces as well? If no, use trim();
+
+function reverseString(sentence) {
+  let a = sentence.split('');
+  let b = [];
+  for (let i = a.length - 1; i >= 0 ; i--){
+    b.push(a[i]);
+  }
+  let c = b.join("");
+  return c;
+}
+
+function reverseString(str){
+  let splitString = str.split('');
+  let reverseArray = splitString.reverse();
+  let joinArray = reverseArray.join('');
+  return joinArray;
+}
+
+function reversString(str){
+  let newString = "";
+  for (var i < str.length - 1; i >= 0; i--){
+    newString += str[i];
+  }
+  return newString;
+}
+*/
